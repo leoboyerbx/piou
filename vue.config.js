397300a77ebb169d094849812ad3a99f3a8e21c8
leoboyerbx <contact@leoboyer.fr>
2020-07-12 17:50:58 +0200

    inital commit of new repo

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    name: 'Piou !',
+    themeColor: '#5D73D8',
+    msTileColor: '#ffffff',
+    iconPaths: {
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
+  }
 }
